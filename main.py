cf07117c88db0e29ee43c143faa19aa77664b578
britt <bmej.schmitz@student.maastrichtuniversity.nl>
2023-11-08 18:25:57 +0100

    final

--- main.py
+++ main.py
@@ -13,32 +13,47 @@ random.seed(3)
 inputs = np.identity(8)
 
 # fit and test the NN with different parameter setups
-errors = []
-for alpha in alphas:
-    for reg_lambda in lambdas:
-        print(f"Training network with alpha = {alpha} and lambda = {reg_lambda}:")
-        network = Network([8, 3, 8], alpha, reg_lambda)
-        history = network.fit(inputs, epochs)
-        # plot training
-        epoch_nums = range(0, epochs, 10)
-        plt.plot(epoch_nums, history)
-        plt.title(f"Training curve for alpha = {alpha} and lambda = {reg_lambda}:")
-        plt.xlabel('epoch')
-        plt.ylabel('MSE')
-        plt.savefig(f'plots/curve_{alpha}_{reg_lambda}.png')
-        plt.show(block=False)
-        plt.clf()
+# errors = []
+# for alpha in alphas:
+#     for reg_lambda in lambdas:
+#         print(f"Training network with alpha = {alpha} and lambda = {reg_lambda}:")
+#         network = Network([8, 3, 8], alpha, reg_lambda)
+#         history = network.fit(inputs, epochs)
+#         # plot training
+#         epoch_nums = range(0, epochs, 10)
+#         plt.plot(epoch_nums, history)
+#         plt.title(f"Training curve for alpha = {alpha} and lambda = {reg_lambda}:")
+#         plt.xlabel('epoch')
+#         plt.ylabel('MSE')
+#         plt.savefig(f'plots/curve_{alpha}_{reg_lambda}.png')
+#         plt.show(block=False)
+#         plt.clf()
+#
+#         print(f"Testing network with alpha = {alpha} and lambda = {reg_lambda}:")
+#         for sample in inputs:
+#             print(f"Input: {sample}")
+#             result = network.test(sample)
+#             print(f"Output: {result.transpose()}")
+#
+#         evaluation = network.evaluate(inputs)
+#         errors.append({"Name" : f"{alpha}_{reg_lambda}", "MSE" : evaluation})
+#
+# results = pd.DataFrame(errors)
+# results.set_index('Name', inplace=True)
+# print(results)
+# results.to_csv("plots/results.csv")
 
-        print(f"Testing network with alpha = {alpha} and lambda = {reg_lambda}:")
-        for sample in inputs:
-            print(f"Input: {sample}")
-            result = network.test(sample)
-            print(f"Output: {result.transpose()}")
+alpha = 0.95
+reg_lambda = 0
 
-        evaluation = network.evaluate(inputs)
-        errors.append({"Name" : f"{alpha}_{reg_lambda}", "MSE" : evaluation})
+network = Network([8, 3, 8], alpha, reg_lambda)
+network.fit(inputs, 10000)
+network.test(inputs[1])
 
-results = pd.DataFrame(errors)
-results.set_index('Name', inplace=True)
-print(results)
-results.to_csv("plots/results.csv")
+print(f"Weights input layer: {network.layers[0].get_thetas()}")
+print(f"Bias input layer: {network.layers[0].get_bias()}")
+
+print(f"Weights hidden layer: {network.layers[1].get_thetas()}")
+print(f"Bias hidden layer: {network.layers[1].get_bias()}")
+
+print(f"Hidden activations for {inputs[1]}: {network.layers[1].get_activations()}")
